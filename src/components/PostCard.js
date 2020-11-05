3a845f08768f293471961572f3cea07ffd55ee66
Dipro56 <sadatshahriar@iut-dhaka.edu>
2020-11-05 16:52:23 +0600

    Create PostCard.js

---
+++ src/components/PostCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View } from "react-native";
+import { Card, Button, Text, Avatar } from "react-native-elements";
+import { AntDesign } from "@expo/vector-icons";
+
+const PostCard = (props) => {
+  return (
+    <Card>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          containerStyle={{ backgroundColor: "#ffab91" }}
+          rounded
+          icon={{ name: "user", type: "font-awesome", color: "black" }}
+          activeOpacity={1}
+        />
+        <Text h4Style={{ padding: 10 }} h4>
+          {props.author}
+        </Text>
+      </View>
+      <Text style={{ fontStyle: "italic" }}> {props.title}</Text>
+      <Text
+        style={{
+          paddingVertical: 10,
+        }}
+      >
+        {props.body}
+      </Text>
+      <Card.Divider />
+      
+    </Card>
+  );
+};
+
+export default PostCard;
